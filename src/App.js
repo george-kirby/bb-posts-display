9de0efbd7c634fc8d4097d6420da8ddfb7ebf737
george-kirby <georgeskirby@btinternet.com>
2019-12-12 20:15:29 +0000

    storing first 10 posts in state and displaying

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import API from './adapters/API'
-import './App.css';
+import React, { useState, useEffect } from "react"
+import API from "./adapters/API"
+import "./App.css"
 
 function App() {
-
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([])
 
   useEffect(() => {
-    // fetches posts and stores in state
-    API.getPosts()
-    .then(console.log)
-  }, []);
+    API.getPosts().then(resp => setPosts(resp.slice(0, 10))) // stores first 10 posts in state
+  }, [])
 
   return (
     <div>
       <h1>Look at all these posts!</h1>
+      <div id="posts-container">
+        {posts.length > 0
+          ? posts.map(post => {
+              return (
+                <div className="post-card">
+                  <h4>{post.title}</h4>
+                  <div>{post.body}</div>
+                </div>
+              )
+            })
+          : "Loading posts"}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
