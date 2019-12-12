c0e12db2256fb466cb2f502ae538ec3d27e9f8fb
george-kirby <georgeskirby@btinternet.com>
2019-12-12 21:04:56 +0000

    added comments

--- src/App.js
+++ src/App.js
@@ -6,21 +6,31 @@ import PostCard from "./components/PostCard"
 function App() {
   const [posts, setPosts] = useState([])
   const [users, setUsers] = useState([])
+  const [comments, setComments] = useState([])
 
   useEffect(() => {
     API.getUsers().then(resp => {
       setUsers(resp)
-      API.getPosts().then(resp => setPosts(resp.slice(0, 10))) // stores first 10 posts in state
+      API.getPosts().then(resp => {
+        setPosts(resp)
+        API.getComments().then(setComments)
+      }) // stores first 10 posts in state
     })
   }, [])
 
   return (
     <div>
-      <h1>Look at all these posts!</h1>
+      <h1>Here are the posts by our wonderful users!</h1>
 
       <div id="posts-container">
         {posts.length > 0
-          ? posts.map(post => <PostCard {...{post}} user={users.find(user => user.id === post.userId)}/>)
+          ? posts.map(post => (
+              <PostCard
+                {...{ post }}
+                user={users.find(user => user.id === post.userId)}
+                comments={comments.filter(comment => comment.postId === post.id)}
+              />
+            ))
           : "Loading posts"}
       </div>
     </div>
